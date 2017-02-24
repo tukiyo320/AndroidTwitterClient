c2a77f8ee0ba3e632e0dadf77fb9503b8a67fdab
tukiyo <tukiyo320@gmail.com>
2017-02-24 15:16:51 +0900

    improve create media view

--- app/src/main/kotlin/jp/co/tukiyo/twitter/ui/adapter/MediaAdapter.kt
+++ app/src/main/kotlin/jp/co/tukiyo/twitter/ui/adapter/MediaAdapter.kt
@@ -1,28 +1,43 @@
 package jp.co.tukiyo.twitter.ui.adapter
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.bumptech.glide.Glide
 import com.twitter.sdk.android.core.models.MediaEntity
 import jp.co.tukiyo.twitter.R
+import jp.co.tukiyo.twitter.databinding.MediaItemBinding
 
 class MediaAdapter(val context: Context) : BaseAdapter() {
     val itemId: Int = R.layout.media_item
     val items : MutableList<MediaEntity> = mutableListOf()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(itemId, parent, false)
-
-        val holder = MediaViewHolder(view).apply {
-            binding.media = getItem(position)
+        val binding : MediaItemBinding
+        if(convertView == null) {
+            binding = DataBindingUtil.inflate(LayoutInflater.from(context), itemId, parent, false)
+            binding.root.tag = binding
+        } else {
+            binding = convertView.tag as MediaItemBinding
         }
-        return holder.binding.root
+
+        return binding.apply {
+            getItem(position)?.let {
+                media = it
+                Glide.with(context)
+                        .load(it.mediaUrl)
+                        .centerCrop()
+                        .fitCenter()
+                        .into(binding.mediaItemImage)
+            }
+        }.root
     }
 
-    override fun getItem(position: Int): MediaEntity {
-        return items[position]
+    override fun getItem(position: Int): MediaEntity? {
+        return items.getOrNull(position)
     }
 
     override fun getItemId(position: Int): Long {
--- app/src/main/kotlin/jp/co/tukiyo/twitter/ui/adapter/MediaViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package jp.co.tukiyo.twitter.ui.adapter
-
-import android.databinding.DataBindingUtil
-import android.view.View
-import jp.co.tukiyo.twitter.databinding.MediaItemBinding
-
-class MediaViewHolder(itemView: View?) {
-    val binding : MediaItemBinding = DataBindingUtil.bind(itemView)
-}
