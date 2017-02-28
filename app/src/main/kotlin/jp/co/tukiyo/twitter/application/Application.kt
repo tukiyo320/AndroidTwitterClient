c8c30c073f53b030982b979b2a508ee8c088b4f7
tukiyo <tukiyo320@gmail.com>
2017-02-28 14:45:14 +0900

    load secrets from local.properties

--- app/src/main/kotlin/jp/co/tukiyo/twitter/application/Application.kt
+++ app/src/main/kotlin/jp/co/tukiyo/twitter/application/Application.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import com.twitter.sdk.android.Twitter
 import com.twitter.sdk.android.core.TwitterAuthConfig
 import io.fabric.sdk.android.Fabric
+import jp.co.tukiyo.twitter.R
 import android.app.Application as AndroidApplication
 
 class Application : AndroidApplication() {
-
-    private val TWITTER_KEY = ""
-    private val TWITTER_SECRET = ""
 
     companion object {
         fun from(context: Context): Application {
@@ -19,7 +17,9 @@ class Application : AndroidApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        val authConfig = TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET)
+        val twitterKey = resources.getString(R.string.twitter_key)
+        val twitterSecret = resources.getString(R.string.twitter_secret)
+        val authConfig = TwitterAuthConfig(twitterKey, twitterSecret)
         Fabric.with(this, Twitter(authConfig))
     }
 }
